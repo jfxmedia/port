a872ce8d28315dc15125e515d996d3ae12229a84
Jesse Foster <jesf777@gmail.com>
2023-08-25 17:13:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-
 // Function to switch project image
 function switchProjectImage(event) {
+    // Remove 'selected' class from all items
+    const projectListItems = document.querySelectorAll('.projects-list ul li');
+    projectListItems.forEach(item => {
+        item.classList.remove('selected');
+    });
+
+    // Add 'selected' class to clicked item
+    event.target.classList.add('selected');
+
     const imgSrc = event.target.getAttribute('data-img');
     if (imgSrc) {
         const projectImage = document.getElementById('project-image');
