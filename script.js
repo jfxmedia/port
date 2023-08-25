7ad1311b98335e63c34a50a98a7c3f5113e37396
Jesse Foster <jesf777@gmail.com>
2023-08-25 16:49:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+// Function to switch project image
+function switchProjectImage(event) {
+    const imgSrc = event.target.getAttribute('data-img');
+    if (imgSrc) {
+        const projectImage = document.getElementById('project-image');
+        projectImage.src = imgSrc;
+        projectImage.alt = event.target.textContent;
+    }
+}
+
+// Event Listener for project list
+document.addEventListener('DOMContentLoaded', function() {
+    const projectListItems = document.querySelectorAll('.projects-list ul li');
+    projectListItems.forEach(item => {
+        item.addEventListener('click', switchProjectImage);
+    });
+});
